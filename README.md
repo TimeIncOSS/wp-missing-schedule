8c836691fb3218ebce92426e08b33e73447a9882
WalterBarcelos <walter.barcelos@timeinc.com>
2016-04-20 17:05:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+WP Missing Schedule
+===================
+
+This plugin creates a wp-cron task to publish the missing scheduled posts.
+
+## Features
+
+- Add wp-cron task which runs every 15min
+- Get all the missing scheduled posts with post_status = future and publish_date < now()
+- Use WordPress core function to publish the list of missing posts
+
+## License
+
+The WP Missing Schedule plugin is licensed under the GPL v2 or later.
+
+> This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+> This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+> You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+## Contributions
+
+Anyone is welcome to contribute to WP Missing Schedule
+
+There are various ways you can contribute:
+
+* Raise an issue on GitHub.
+* Send us a Pull Request with your bug fixes and/or new features.
+* Provide feedback and suggestions on enhancements.
+* Submit plugin translations 
+
+## Credits 
+
+This plugin was written by the development team at [Time Inc. (UK) Ltd](http://www.timeincuk.com/) and maintained by [Walter Barcelos](https://github.com/walterbarcelos/).
---
+++ wp-missing-schedule.php
@@ -0,0 +1,306 @@
+<?php
+/**
+ * WP Missing Schedule
+ *
+ * Publish missing scheduled posts
+ *
+ * @package   WP_Missing_Schedule
+ * @author    Walter Barcelos <walter.barcelos@timeinc.com>
+ * @license   GPL-2.0+
+ * @link      http://www.timeincuk.com/
+ * @copyright 2016 Time Inc. (UK) Ltd
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Missing Schedule
+ * Plugin URI:        http://www.timeincuk.com/
+ * Description:       Publish missing scheduled posts
+ * Version:           1.0.0
+ * Author:            Walter Barcelos
+ * Author URI:        http://walterbarcelos.com/
+ * Text Domain:       wp-missing-schedule
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /lang
+ * GitHub Plugin URI: https://github.com/TimeIncUK/wp-missing-schedule
+ */
+
+
+// Prevent direct file access
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+class WP_Missing_Schedule {
+
+	/**
+	 * Instance of this class.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      object
+	 */
+	protected static $instance = null;
+
+	/**
+	 *
+	 * The variable name is used as the text domain when internationalizing strings
+	 * of text. Its value should match the Text Domain file header in the main
+	 * widget file.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      string
+	 */
+	protected $plugin_slug = 'wp-missing-schedule';
+
+	/**
+	 *
+	 * Version number.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @var      string
+	 */
+	protected $version = '1.0.0';
+
+	/**
+	 * Specifies the classname and description, instantiates the widget,
+	 * loads localization files, and includes necessary stylesheets and JavaScript.
+	 */
+	public function __construct() {
+
+		add_action( 'admin_init', array( $this, 'maybe_upgrade' ) );
+
+		// load plugin text domain
+		add_action( 'init', array( $this, 'plugin_textdomain' ) );
+
+		// Activate plugin when new blog is added
+		add_action( 'wpmu_new_blog', array( $this, 'activate_new_site' ) );
+
+		add_filter( 'cron_schedules', array( $this, 'cron_add_quarter_hourly' ) );
+		add_action( 'wp_publish_missing_schedule_posts', array( $this, 'wp_missing_schedule_posts' ) );
+
+	} // end constructor
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @since     1.0.0
+	 *
+	 * @return    object    A single instance of this class.
+	 */
+	public static function get_instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	public function single_activate() {
+		$op = 'wp_missing_schedule_posts_dbv';
+		$this->schedule_publish_missing_posts();
+		update_option( $op, $this->version );
+	}
+
+	/**
+	 * Fired when the plugin is activated.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    boolean $network_wide       True if WPMU superadmin uses
+	 *                                       "Network Activate" action, false if
+	 *                                       WPMU is disabled or plugin is
+	 *                                       activated on an individual blog.
+	 */
+	public function activate( $network_wide ) {
+
+		if ( $network_wide && function_exists( 'is_multisite' ) && is_multisite() ) {
+			// Get all blog ids of the current network
+			$sites = wp_get_sites();
+
+			foreach ( $sites as $id => $site ) {
+				switch_to_blog( $site[ 'blog_id' ] );
+				$this->single_activate();
+			}
+			restore_current_blog();
+		} else {
+			$this->single_activate();
+		}
+
+	}
+
+	public function single_deactivate() {
+		$this->unschedule_publish_missing_posts();
+	}
+
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    boolean $network_wide       True if WPMU superadmin uses
+	 *                                       "Network Deactivate" action, false if
+	 *                                       WPMU is disabled or plugin is
+	 *                                       deactivated on an individual blog.
+	 */
+	public function deactivate( $network_wide ) {
+
+		if ( $network_wide && function_exists( 'is_multisite' ) && is_multisite() ) {
+			// Get all blog ids of the current network
+			$sites = wp_get_sites();
+
+			foreach ( $sites as $id => $site ) {
+				switch_to_blog( $site[ 'blog_id' ] );
+				$this->single_deactivate();
+			}
+			restore_current_blog();
+		} else {
+			$this->single_deactivate();
+		}
+
+	}
+
+
+	/**
+	 * Check version number to decide if schedule a publishing task
+	 *
+	 * @return void
+	 */
+	public function maybe_upgrade() {
+		$op  = 'wp_missing_schedule_posts_dbv';
+		$ver = get_option( $op, '0.0.0' );
+		if ( version_compare( $ver, '1.0.0', '<' ) ) {
+			$this->single_activate();
+		}
+	}
+
+	/**
+	 * Fired when a new site is activated with a WPMU environment.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param    int $blog_id ID of the new blog.
+	 *
+	 * @return void
+	 */
+	public function activate_new_site( $blog_id ) {
+
+		if ( 1 !== did_action( 'wpmu_new_blog' ) ) {
+			return;
+		}
+
+		switch_to_blog( $blog_id );
+		$this->single_activate();
+		restore_current_blog();
+
+	}
+
+	/**
+	 * Creates a custom wp cron schedule interval (15min)
+	 *
+	 * @param array $schedules Intervals pre defined in WordPress
+	 *
+	 * @return array $schedules Intervals plus our custom value
+	 */
+	public function cron_add_quarter_hourly( $schedules ) {
+		// Adds twice hourly to the existing schedules, if it's not there.
+		if ( false === array_key_exists( 'quarterhourly', $schedules ) ) {
+			$schedules[ 'quarterhourly' ] = array(
+				'interval' => 900,
+				'display'  => __( 'Quarter Hourly' ),
+			);
+		}
+
+		return $schedules;
+	}
+
+	/**
+	 * Publish the missing scheduled posts
+	 *
+	 * @return void
+	 */
+	public function wp_missing_schedule_posts() {
+
+		// Get the missing scheduled posts from the database
+		$args = array(
+			'post_status' => 'future',
+			'date_query'  => array(
+				array(
+					'before' => date( 'Y-m-d H:i:s' ),
+				),
+			),
+		);
+
+		$scheduled_posts = new WP_Query( $args );
+
+		while ( $scheduled_posts->have_posts() ) : $scheduled_posts->the_post();
+			check_and_publish_future_post( get_the_ID() );
+		endwhile;
+
+		wp_reset_postdata();
+
+	}
+
+	/**
+	 * Schedule a wp cron event to fetch all the rss urls every 30min
+	 *
+	 * @return void
+	 */
+	public function schedule_publish_missing_posts() {
+		wp_schedule_event( time(), 'quarterhourly', 'wp_publish_missing_schedule_posts' );
+	}
+
+	/**
+	 * Unschedule the wp cron event to fetch all the rss urls every 30min
+	 *
+	 * @return void
+	 */
+	public function unschedule_publish_missing_posts() {
+		$op = 'wp_missing_schedule_posts_dbv';
+		delete_option( $op );
+		wp_clear_scheduled_hook( 'wp_publish_missing_schedule_posts' );
+	}
+
+	/**
+	 * Return the plugin slug.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return   string Plugin slug variable.
+	 */
+	public function get_plugin_slug() {
+		return $this->plugin_slug;
+	}
+
+	/*--------------------------------------------------*/
+	/* Public Functions
+	/*--------------------------------------------------*/
+	/**
+	 * Loads the Widget's text domain for localization and translation.
+	 */
+	public function plugin_textdomain() {
+		load_plugin_textdomain( $this->get_plugin_slug(), false, plugin_dir_path( __FILE__ ) . 'lang/' );
+	}
+
+} // end class
+
+
+function wp_missing_schedule_activate( $network_wide ) {
+	$object = new WP_Missing_Schedule();
+	$object->activate( $network_wide );
+}
+
+register_activation_hook( __FILE__, 'wp_missing_schedule_activate' );
+
+function wp_missing_schedule_deactivate( $network_wide ) {
+	$object = new WP_Missing_Schedule();
+	$object->deactivate( $network_wide );
+}
+
+register_deactivation_hook( __FILE__, 'wp_missing_schedule_deactivate' );
+
+add_action( 'plugins_loaded', array( 'WP_Missing_Schedule', 'get_instance' ) );
